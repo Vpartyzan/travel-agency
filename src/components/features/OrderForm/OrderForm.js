de997298af0b54b0cabf16c3638ee25a1405299e
vPartyzan <peregrim.mt@gmail.com>
2021-04-03 14:32:31 +0200

    Add json-server && Add sending tripData to API

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -3,11 +3,45 @@ import PropTypes from 'prop-types';
 import { Row, Col } from 'react-flexbox-grid';
 
 import Pricing from '../../../data/pricing.json';
+import settings from '../../../data/settings';
+
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
 
 import OrderSummary from '../OrderSummary/OrderSummary';
 import OrderOption from '../OrderOption/OrderOption';
+import Button from '../../common/Button/Button';
 
-const OrderForm = ({ tripCost, options, setOrderOption }) => (
+const sendOrder = (options, tripCost, tripData) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    tripId: tripData.id,
+    tripName: tripData.name,
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
+
+const OrderForm = ({ tripCost, options, setOrderOption, tripData }) => (
   <Row>
     {Pricing.map( opt => (
       <Col key={opt.id} md={4}>
@@ -17,6 +51,7 @@ const OrderForm = ({ tripCost, options, setOrderOption }) => (
     <Col xs={12}>
       <OrderSummary tripCost={tripCost} options={options}/>
     </Col>
+    <Button onClick={() => (options.contact == '' && options.name == '') ? alert('Name and contact info should not be empty.') : sendOrder(options, tripCost, tripData) }>Order now!</Button>
   </Row>
 );
 
@@ -24,6 +59,7 @@ OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.object,
   setOrderOption: PropTypes.func,
+  tripData: PropTypes.object,
 };
 
 export default OrderForm;
